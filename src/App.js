84e30955d9651f1b23193924fcab2ca180a46a8f
kim <laylaone22@gmail.com>
2021-09-24 13:49:27 +0200

    sass structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="title">OIKIA</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="title">OIKIA</h1>
+        </div>
+    );
 }
 
 export default App;
