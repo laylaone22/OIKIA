2c385a3ca17a4b74e4497e9537d456907e20cf24
GiulianoMarcoMontis <giulianomarco.montis@gmail.com>
2021-10-06 09:42:11 +0200

    added logic in login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,4 +1,56 @@
+import { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { authContext } from '../stores/auth/auth';
+
 const Login = () => {
+    const history = useHistory();
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [hasLoginError, setHasLoginError] = useState(false);
+
+    const { setIsLoggedIn, setUserData, setAuthToken } = useContext(authContext);
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+
+        try {
+            const res = await fetch('http://localhost:3000/users/login', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            const data = await res.json();
+
+            if (res.ok) {
+                const token = res.headers.get('x-auth-token');
+                setIsLoggedIn(true);
+                setUserData(data);
+                setAuthToken(token);
+                localStorage.setItem('isLoggedIn', JSON.stringify(true));
+                localStorage.setItem('userData', JSON.stringify(data));
+                localStorage.setItem('authToken', JSON.stringify(token));
+                history.push('/userdashboard');
+            } else {
+                // 400 status codes are not errors with fetch
+                const error = new Error('Validation failed');
+                error.error = data.error;
+                throw error;
+            }
+        } catch (err) {
+            // 400s and 500s are handled here
+            setHasLoginError(true);
+        }
+    };
+
+    const handleChange = ({ target: { name, value } }) => setFormData({ ...formData, [name]: value });
+
     return (
         <div className="Login">
             <main className="Login__body">
@@ -7,7 +59,8 @@ const Login = () => {
                 </header>
 
                 <section className="Login__body--search__form">
-                    <form>
+                    <form onSubmit={handleSubmit}>
+                        {hasLoginError && <p className="error">Error: E-mail or password incorrect</p>}
                         <div>
                             <label htmlFor="email" className="Login__body--search__form__label">
                                 <input
@@ -16,8 +69,8 @@ const Login = () => {
                                     name="email"
                                     placeholder="Email"
                                     required
-                                    // value={formData.email}
-                                    // onChange={handleChange}
+                                    value={formData.email}
+                                    onChange={handleChange}
                                     className="Login__body--search__form__input"
                                 />
                             </label>
@@ -30,8 +83,8 @@ const Login = () => {
                                     name="password"
                                     placeholder="Password"
                                     required
-                                    // value={formData.password}
-                                    // onChange={handleChange}
+                                    value={formData.password}
+                                    onChange={handleChange}
                                     className="Login__body--search__form__input"
                                 />
                             </label>
