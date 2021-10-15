adb6c943aabe40722c757b86eee8998036d3ae63
GiulianoMarcoMontis <giulianomarco.montis@gmail.com>
2021-10-15 17:25:58 +0200

    major styling adjustments to plantcyclopedia and myFavorites views

--- src/views/MyFavorites.js
+++ src/views/MyFavorites.js
@@ -1,16 +1,74 @@
-import { useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router';
 
 // contexts
-import { authContext } from '../stores/auth/auth.js';
+import { authContext } from '../stores/auth/auth';
 import { dataContext } from '../stores/data/store';
 
+// actions to dispatch
+import { REMOVE_FAVORITE } from '../stores/data/actions';
+
 // components
-import FavoriteCard from '../components/FavoriteCard.js';
+import MyFavoriteCard from '../components/MyFavoriteCard.js';
+
+// assets
+import addFav from '../assets/icons/ui/addFav.svg';
+import favorites from '../assets/icons/ui/favorites.svg';
 
 const MyFavorites = () => {
-    // context auth data
-    const { userData } = useContext(authContext);
+    const history = useHistory();
+
+    // contexts
+    const { userData, authToken } = useContext(authContext);
     const { dataState, dispatch } = useContext(dataContext);
+
+    // state
+    const [favoritesToPut, setFavoritesToPut] = useState({ myFavorites: [] });
+
+    // PUT favorites ID to BE on dataState.myFavorites change
+    useEffect(() => {
+        console.log('myFavorites View - useEffect runs on dataState.myFav change!!');
+        // take all favorites in dataState and make an array out of the IDs
+        const favoritesData = { ...favoritesToPut, myFavorites: dataState.myFavorites.map(({ _id }) => _id) };
+
+        // POST the array of myFavorite IDs
+        const updateMyFavorites = async () => {
+            const URL = `http://localhost:3000/users/${userData._id}`;
+
+            const OPTIONS = {
+                method: 'PUT',
+                body: JSON.stringify(favoritesData),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-auth-token': authToken
+                }
+            };
+
+            try {
+                const response = await fetch(URL, OPTIONS);
+                const data = await response.json();
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        updateMyFavorites();
+
+        // run every time myFavorites changes
+    }, [dataState.myFavorites]);
+
+    // removeFavorites happens in the MyFavoriteCard:
+    // dispatches to dataState
+    // set favorites IDs to send to BE
+
+    const removeFavorite = (plant) => {
+        // to remove favorite we need the plant._id to filter it out of the dataState
+        dispatch({ type: REMOVE_FAVORITE, payload: plant._id });
+
+        // set favoritesToPut state for the useEffect to PUT them
+        const favoritesData = { ...favoritesToPut, myFavorites: dataState.myFavorites.map(({ _id }) => _id) };
+        setFavoritesToPut(favoritesData);
+    };
 
     return (
         <div className="MyFavorites">
@@ -20,11 +78,21 @@ const MyFavorites = () => {
                 </header>
                 <section className="MyFavorites__body__favorites">
                     <h2>Your Favorite Plants</h2>
-                    {dataState.myFavorites?.map((fav, i) => (
-                        <FavoriteCard key={fav._id} favorite={fav} delay={i} />
+
+                    {dataState.myFavorites?.map((favorite, i) => (
+                        // <FavoriteCard key={fav._id} favorite={fav} delay={i} />
+                        <MyFavoriteCard
+                            key={favorite._id}
+                            favorite={favorite}
+                            delay={i}
+                            removeFavorite={removeFavorite}
+                        />
                     ))}
                 </section>
             </main>
+            <div className="MyFavorites__addFavorites">
+                <img src={addFav} alt="add more favorites" onClick={() => history.push('/plantcyclopedia')} />
+            </div>
         </div>
     );
 };
